57b9def2514a2a545d2b9c98902aa4657525e371
NtlX <github@manueldewald.de>
2017-08-21 21:01:05 +0200

    arduino code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ScrumTimer
+
+This project is a timer for scrum daily standups.
+Based on team size and needs the timer can be configured from 1 to 99 seconds. The current time left is shown on a 2 digit 7 SEG display.
+To start or stop the timer, a touch module can be pressed.
+The logic is implemented for Arduino.
+Arduino Nano is suitable for this project, the 3D printing models (soon to come) are also designed for the size of an Arduino Nano.
---
+++ arduino/scrum.ino
@@ -0,0 +1,116 @@
+#include "pitches.h"
+#include <LEDDisplay.h>
+//Tone generation based on https://www.arduino.cc/en/Tutorial/toneMelody
+
+LEDDisplay *led;
+unsigned long lastUpdate = 0;
+int counter;
+ 
+int melody[] = {
+  NOTE_C6, NOTE_CS6, NOTE_D6, 0, 0, NOTE_G4, NOTE_F5, 0, NOTE_F5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_E4, 0, NOTE_E4, NOTE_C4
+};
+
+int noteDurations[] = {
+  16, 16, 8, 4, 2, 4, 4, 4, 4, 3, 3, 3, 4, 4, 4, 4, 4
+};
+
+void playMelody() {
+  for (int thisNote = 0; thisNote < 17; thisNote++) {
+    int noteDuration = 700 / noteDurations[thisNote];
+    tone(12, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(2);
+  }
+}
+
+void setup() {
+  int digitFlagPins[] = {10, 11};
+  int segmentPins[] = {2, 3, 4, 5 ,6 ,7 ,8, 9};
+  int decimalPointPin = 9;
+  led = new LEDDisplay(2, digitFlagPins, segmentPins, decimalPointPin);
+  pinMode(13, INPUT);
+}
+
+
+bool startPressed() {
+  int state = digitalRead(13);
+  return state == HIGH;
+}
+
+void showNumber(int number) {
+    for(int i = 0; i < 2; i++)
+    {
+        led->displayNumber(number % 10, i);
+        delay(2);
+        number = number / 10;
+    }
+}
+bool finished = true;
+void updateCounter() {
+    if (counter == 0)
+    {
+      playMelody();
+      resetCounter();
+      finished = true;
+    }
+    
+    unsigned long now = millis();
+    if (now - lastUpdate > 1000)
+    {
+        lastUpdate = now;
+        counter--;
+    }
+}
+
+int readStart() {
+  int number = (analogRead(A0) -100)*0.15;
+  if (number > 99) {
+    return 99;
+  } else if (number < 0){
+    return 0;
+  }
+  return number;
+}
+
+int start = 0;
+void resetCounter() {
+  lastUpdate = millis();
+  start = readStart();
+  counter = start;
+}
+
+int lastButtonUpdate = 0;
+void checkStartPressed() {
+  if (startPressed()) {
+    unsigned long now = millis();
+    if (now - lastButtonUpdate > 1000)
+    {
+      resetCounter();
+      lastButtonUpdate = now;
+      finished = !finished;
+    }
+  }
+}
+
+void checkPotiMoved() {
+  int poti = readStart();
+  int diff = abs(poti - start);
+  if (diff > 20) {
+    resetCounter();
+    finished = true;
+  }
+}
+
+void loop() {
+  noTone(12);
+  if (!finished) {
+    showNumber(counter);
+    updateCounter();
+    checkPotiMoved();
+  } else {
+    showNumber(readStart());
+  }
+  checkStartPressed();
+}
